7b6243989637ba61bf5b096e2548448da2567a4b
Chris Francis <francis.chris5@gmail.com>
2023-09-07 18:57:15 -0400

    fixed abstract class

--- Critters/src/critters/Zoo.java
+++ Critters/src/critters/Zoo.java
@@ -4,17 +4,25 @@ package critters;
 
 public class Zoo {
     public static void main(String[] args){
+        Zoo zoo = new Zoo();
+        
         Animal buddy = new Dog("Buddy", 5, 34.2, true);
         System.out.println(buddy);
+        zoo.listen(buddy);
         System.out.println(Animal.getAnimalCounter());
+        
+        //Animal something = new Animal();
+        
         
         
         Animal simba = new Lion("Simba", 3, 600);
         System.out.println(simba);
+        zoo.listen(simba);
         System.out.println(Animal.getAnimalCounter());
         
         Fish fishy = new Fish("Fishy", 2, 0.75);
         Animal nemo = new Fish();
+        zoo.listen(nemo);
         nemo.setAge(3);
         nemo.setName("Nemo");
         nemo.setWeight(0.5);
@@ -27,5 +35,10 @@ public class Zoo {
         }
         
         Cat fluffy = new Cat("fluffy", 5, 12.3);
+        zoo.listen(fluffy);
+    }
+    
+    public void listen(Animal animal){
+        System.out.println(animal.makeSound());
     }
 }
