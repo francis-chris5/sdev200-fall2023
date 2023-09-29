518f79027b3fed563b149e26422b5c0bc8dcab6f
Chris Francis <francis.chris5@gmail.com>
2023-09-29 12:26:07 -0400

    fixed deprecated URL

--- Chapter34Homework/src/mysqlconnection/RetrieveData.java
+++ Chapter34Homework/src/mysqlconnection/RetrieveData.java
@@ -3,6 +3,7 @@ package mysqlconnection;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 
@@ -12,9 +13,11 @@ public class RetrieveData {
     private BufferedReader dataStream;
     private ArrayList<String[]> lines = new ArrayList<>(); //0 is id, 1 is boy name, 2 is boy count, 3 is girl name, 4 is girl count
 
-    public RetrieveData(String babyNamesSite) {
+    public RetrieveData(String babyNamesSiteString) {
         try {
-            this.babyNamesSite = new URL(babyNamesSite);
+            // URL constructors were deprecated last Sunday 9-24-2023, type still exists, but need instantiated this way
+            //this.babyNamesSite = new URL(babyNamesSiteString);
+            this.babyNamesSite = URI.create(babyNamesSiteString).toURL();
             dataStream = new BufferedReader(new InputStreamReader(this.babyNamesSite.openStream()));
             while(true){
                 String nextLine = dataStream.readLine();
